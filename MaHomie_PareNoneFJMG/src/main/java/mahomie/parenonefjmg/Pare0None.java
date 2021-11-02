6b9ac7bbf5745a44d4b8e183c58cdc9aa41f3c1e
Xelemental <xelemental864@gmail.com>
2021-11-02 13:05:33 +0100

    Versión A + B (Option Select)

--- MaHomie_PareNoneFJMG/src/main/java/mahomie/parenonefjmg/Pare0None.java
+++ MaHomie_PareNoneFJMG/src/main/java/mahomie/parenonefjmg/Pare0None.java
@@ -23,7 +23,18 @@ public class Pare0None {
         char choice;
         Random slap = new Random();
         
-        do {//Pedimos manualmente los dedos (un nº de dedos
+        do {
+
+            //Pedimos por pantalla si los números son introducidos o RNG
+            System.out.println("¿Cómo quiere introducir los números?\n"
+                    + "A (Manual)\n" + "B (Al azar)");
+
+            choice = entry.next().charAt(0);
+
+            switch (choice) {
+
+                case 'A':
+                    do {//Pedimos manualmente los dedos (un nº de dedos
                         //por jugador)
                         System.out.println("Jugador 1, ¿cuántos dedos contamos?");
                         j1 = entry.nextInt();
@@ -38,7 +49,33 @@ public class Pare0None {
                         //"¿Lo introducido se sale del rango (0-10)? 
                         //PERMÍTEME QUE INSISTA"
                     } while (j1 < 11 || j1 < 0 || j2 < 11 || j2 < 0);
-        
+
+                    break;
+
+                case 'B':
+
+                    System.out.println("De acuerdo, se generarán los números "
+                            + "al azar.");
+
+                    j1 = slap.nextInt(11);
+                    System.out.println("J1 = " + j1 + " dedos");
+
+                    j2 = slap.nextInt(11);
+                    System.out.println("J2 = " + j2 + " dedos");
+
+                    break;
+
+                default://Si lo introducido no coincide con las 2 opciones
+                    //Salta error
+                    System.out.println("ERROR. Reseteando programa.");
+
+                    break;
+
+            }
+
+            //"¿No coincide la opción con lo introducido? PERMÍTEME QUE INSISTA"
+        } while (choice != 'A' && choice != 'B');
+
         //Se suman los números y se muestra si ha salido PARES o NONES
         n = j1 + j2;
 
